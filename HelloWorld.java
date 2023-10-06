480a80d2724047ea5be8e165bf79718eff0062ba
Vivan Poddar <vivanneil@outlook.com>
2023-10-06 15:59:48 -0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package CPCS; //remove this line or change it to your own package name (the enclosing folder of this file)
+
+public class HelloWorld { 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Templates/template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ld long double
+#define ll long long
+#define endl "\n"
+#define pi 3.14159265
+#define Gojo ios::sync_with_stdio(0), ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+void setIO(string s)
+{
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int main() {
+    setIO("ChooseAFilename");
+    // use cin or scanf to take strings, delimited by a space
+    // cout will automatically write to the file
+}
